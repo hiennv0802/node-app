181d40645d6e3611a099c3e824194b90de691438
hiennv94 <hiennv.th@gmail.com>
2016-12-19 23:28:33 +0700

    Inprogress

--- app.js
+++ app.js
@@ -3,22 +3,16 @@ var path = require('path');
 var app = express();
 var http = require('http');
 var fs = require('fs');
-var mongodb = require("mongodb");
-var MongoClient = mongodb.MongoClient;
-
+var mongodb = require('mongodb');
+var swig = require('swig');
+var swig = new swig.Swig();
 var db =require('mongoose');
-// MongoClient.connect('mongodb://localhost:27017/node-app');
+
 db.connect('mongodb://localhost:27017/node-app');
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-// app.use(express.favicon());
-// app.use(express.logger('dev'));
-// app.use(express.bodyParser());
-// app.use(express.methodOverride());
-// app.use(express.cookieParser('your secret here'));
-// app.use(express.session());
-// app.use(app.router);
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
 
 fs.readdirSync('./controllers/').forEach(function (file) {
@@ -28,13 +22,6 @@ fs.readdirSync('./controllers/').forEach(function (file) {
   }
 });
 
-// http.createServer(app).listen(app.get('port'), function() {
-//   console.log('Express server is listening on port ' + app.get('port'));
-// });
-
-// app.listen(3000, function() {
-//   console.log("Listen on port 3000...");
-// });
 app.listen(3000);
 
 module.exports = app;
--- controllers/users.js
+++ controllers/users.js
@@ -2,15 +2,23 @@ var express = require('express')
   , router = express.Router()
   , User = require('../models/users')
   , mongoose = require('mongoose');
-// var mongodb = require("mongodb");
-// var MongoClient = mongodb.MongoClient;
 
 module.exports.controller = function(app) {
   app.get('/users', function(req, res) {
-    // var User = mongoose.model(User, mongoose);
-    // users = User.findOne({ username: 'Hien Nguyen' })
-    // console.log(users)
-    // console.log('aaaaaa')
-    res.render('users/index', {name: 'Hien Nguyen'});
+
+    User = mongoose.model('user');
+    users = User.find();
+    console.log(users);
+    res.format({
+      html: function() {
+        res.render('users/index.html', {
+          title: "All users",
+          users: users
+        });
+      },
+      json: function() {
+        res.json({name: 'Nguyen Van Hien'});
+      }
+    });
   });
 }
