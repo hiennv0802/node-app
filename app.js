b4393d60144e50f9c0b6cddcbe14fc22235730ce
hiennv94 <hiennv.th@gmail.com>
2016-12-21 23:38:33 +0700

    Create user

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var mongodb = require('mongodb');
 var swig = require('swig');
 var swig = new swig.Swig();
 var db = require('mongoose');
+var bodyParser = require('body-parser');
 
 db.connect('mongodb://localhost:27017/node-app');
 app.set('port', process.env.PORT || 3000);
@@ -14,6 +15,10 @@ app.set('views', __dirname + '/views');
 app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/css', express.static(__dirname + '/public/stylesheets/style.css'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 fs.readdirSync('./controllers/').forEach(function (file) {
   if(file.substr(-3) == '.js') {
